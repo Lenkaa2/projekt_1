ace5fc7a427c60b3576d4c08c3a2e69a5dbdb4e8
Lenkaa2 <lenka.pazourova@seznam.cz>
2023-10-09 20:28:06 +0200

    Users and passwords

--- projekt_1.py
+++ projekt_1.py
@@ -6,3 +6,26 @@ email: lenka.pazourova@seznam.cz
 discord: LenkaP
 """
 
+import task_template
+registered_users = ['bob', 'ann', 'mike', 'liz']
+users_passwords = {
+                    'bob': '123', 
+                    'ann': 'pass123', 
+                    'mike': 'password123', 
+                    'liz': 'pass123'
+                    }
+separator = '-' * 40
+
+user = input("Username: ")
+if user not in registered_users:
+    print("Unregistered user. Terminating program..")
+
+else:
+    password = input("Password: ")
+    if users_passwords.get(user) == password:
+        print(separator)
+        print(f"Welcome to the app, {user}\nWe have 3 texts to be analyzed.")
+        print(separator)
+    
+    else: 
+        print("Wrong password, try again.")
